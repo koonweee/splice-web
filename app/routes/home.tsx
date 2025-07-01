743487978ddc325a3ff68a53b9cc94189cff8244
Jeremy <jeremytkw98@gmail.com>
2025-07-01 17:01:33 +0800

    cleanup

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "splice-web" },
+    { name: "description", content: "simple frontend for splice" },
   ];
 }
 
